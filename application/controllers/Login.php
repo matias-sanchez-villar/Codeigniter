251e87329cf7e60fc6ba073d05fb05cc69d830cb
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2021-01-15 04:08:19 -0300

    login pero con errores de URL

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,14 +5,25 @@ class Login extends CI_Controller {
 
 	public function __construct (){
 		parent::__construct ();
-		$this->load->helper(array("menu", "url"));
+		$this->load->helper(array("menu", "url", 'autentificacion/login'));
+		$this->load->library('form_validation');
 	}
-	
+
 	public function index(){
 		$data["Menu"]=main_menu();
 		$this->load->view("head");
 		$this->load->view("navbar", $data);
 		$this->load->view("login");
 		$this->load->view("footer");
+	}
+
+	public function validate(){
+		$rules=getLogin();
+		$this->form_validation->set_rules($rules);
+		if($this->form_validation->run()===FALSE){
+			$this->load->view("login");
+		}else {
+			$this->index();
+		}
 	}
 }
--- application/views/login.php
+++ application/views/login.php
@@ -5,14 +5,15 @@
 
     <div class="container">
         <h2>Ingreso: </h2>
-        <form action="/action_page.php">
+        <?= validation_errors(); ///mensaje de error ?>
+        <form action="<?= base_url("Login/validate"); ?>">
             <div class="form-group">
                 <label for="email">Email:</label>
                 <input type="email" class="form-control" id="email" placeholder="mail" name="email">
             </div>
             <div class="form-group">
                 <label for="pwd">Contraseña:</label>
-                <input type="password" class="form-control" id="pwd" placeholder="Contraseña" name="Pasword">
+                <input type="password" class="form-control" id="pwd" placeholder="Contraseña" name="password">
             </div>
             <button type="submit" class="btn btn-primary">Ingresar</button>
         </form>
